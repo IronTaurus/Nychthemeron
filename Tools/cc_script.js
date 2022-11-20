4bfd923c22e05f31b263b2024f05111f3fd2ecbd
IronTaurus <IronTaurus@users.noreply.github.com>
2022-11-20 14:28:57 +0100

    update art

--- Tools/cc_script.js
+++ Tools/cc_script.js
@@ -6,6 +6,7 @@
     var classDropdown = document.getElementById("classSubtypes");
     var tierDropdown = document.getElementById("classTierSelect");
     var baseClass = "";
+    var imgPath = "../Art/"
     
     function selectClass(selectedClass){
         const $select = document.querySelector('#classSubtypes');
@@ -86,7 +87,7 @@
                     }
                     else{
                         document.getElementById("rangeArt_sun").style.display = "block";
-                        document.getElementById("rangeArt_sun").src = "/Art/range" + element.value + ".png"
+                        document.getElementById("rangeArt_sun").src = "range" + element.value + ".png"
                     }                   
                 }
                 if(element.name == "cmRangeType"){
@@ -95,7 +96,7 @@
                     }
                     else{
                         document.getElementById("rangeArt_moon").style.display = "block";
-                        document.getElementById("rangeArt_moon").src = "/Art/range" + element.value + ".png"
+                        document.getElementById("rangeArt_moon").src = imgPath + "range" + element.value + ".png"
                     }                 
                 }             
             }          
@@ -103,15 +104,15 @@
         //Replace all instances of keywords with a image.            
         const mapObj = 
         {
-            '{ac}': "<img src='/Art/Symbol_AC.png' height = '20px' width = '15px' margin= '0%'/>",
-            '{w}': "<img src='/Art/BaseDmg.png' height = '22' width = '17' />",
-            '{and}': "<img src='/Art/and.png' height='16' width='340' style='margin-top: 8px'/> <br>",
-            '{or}': "<img src='/Art/or.png' height='16' width='340' style='margin-top: 8px'/> <br>",
-            '{-}': "<img src='/Art/break.png' height='16' width='340'/> <br>",
-            '{one}': "<img src='/Art/break_first.png' height='16' width='340'/> <br>",
-            '{two}': "<img src='/Art/break_second.png' height='16' width='340'/> <br>",
-            '{three}': "<img src='/Art/break_third.png' height='16' width='340'/> <br>",
-            '{melody}': "<img src='/Art/LingeringMelody.png' height='16' width='340'/> <br>",
+            '{ac}': "<img src='"+ imgPath +"Symbol_AC.png' height = '20px' width = '15px' margin= '0%'/>",
+            '{w}': "<img src='"+ imgPath +"BaseDmg.png' height = '22' width = '17' />",
+            '{and}': "<img src='"+ imgPath +"and.png' height='16' width='340' style='margin-top: 8px'/> <br>",
+            '{or}': "<img src='"+ imgPath +"or.png' height='16' width='340' style='margin-top: 8px'/> <br>",
+            '{-}': "<img src='"+ imgPath +"break.png' height='16' width='340'/> <br>",
+            '{one}': "<img src='"+ imgPath +"break_first.png' height='16' width='340'/> <br>",
+            '{two}': "<img src='"+ imgPath +"break_second.png' height='16' width='340'/> <br>",
+            '{three}': "<img src='"+ imgPath +"break_third.png' height='16' width='340'/> <br>",
+            '{melody}': "<img src='"+ imgPath +"LingeringMelody.png' height='16' width='340'/> <br>",
         };
         let sunRuleReplaced = sunRuleText.replace(/{ac}|{w}|{and}|{or}|{-}|{one}|{two}|{three}|{melody}/gi, matched => mapObj[matched]);
         let moonRuleReplaced = moonRuleText.replace(/{ac}|{w}|{and}|{or}|{-}|{one}|{two}|{three}|{melody}/gi, matched => mapObj[matched]);
