5cd68e7d5af9c3831629c8ed337b2d4bc4425d44
IronTaurus <IronTaurus@users.noreply.github.com>
2023-06-22 14:48:55 +0200

    Download function

--- Tools/sheet_script.js
+++ Tools/sheet_script.js
@@ -2,6 +2,31 @@
 var profileArt = document.getElementById("ProfileArt");
 const artHeight = profileArt.height;
 const artWidth = profileArt.width;
+const characterSheet = document.querySelector("#CharacterSheet");
+
+function draw2canvas(){
+    console.log("Test draw2canvas complete")
+    const characterName = document.getElementById("p_Name").value.replace(/\s+/g, '');
+    const className = document.getElementById("p_Class").value.replace(/\s+/g, '');
+    const fileName =  characterName + "_" + className;
+    html2canvas(characterSheet, {
+        scale: 3,
+        background :'#FFFFFF',
+        useCORS: true,
+    }).then(canvas => {
+        canvas.id = "h2Canvas";
+        document.body.appendChild(canvas);
+        download_img(canvas.id, fileName);       
+    });
+}
+function download_img(canvasId, fileName) {
+    var link = document.createElement('a');
+    link.download = fileName + ".png";
+    link.href = document.getElementById(canvasId).toDataURL()
+    link.click();
+    var canvasElement = document.getElementById(canvasId);
+    document.body.removeChild(canvasElement);  
+};
 
 function Resize(slider){
     console.log("Resize")
